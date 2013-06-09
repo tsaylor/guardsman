844ca9d8b404b29844eb7ec0433e31acf758bd0d
Tim Saylor <tim.saylor@gmail.com>
2013-06-09 01:55:05 -0500

    reorganized repo, added settings file

--- activation_server.py
+++ activation_server.py
@@ -1,23 +1,27 @@
-import RPi.GPIO as GPIO
 import time
-import serial
 import urllib2
-from wiegand import serial_to_wiegand
+import argparse
+
+import yaml
+import serial
+import RPi.GPIO as GPIO
+from lib.wiegand import serial_to_wiegand
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--settings", help="Path to the settings file", default="conf/settings.yaml")
+args = parser.parse_args()
+
+settings = yaml.load(open(args.settings))
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)  # controls the relay for tool activation
-
-RESOURCE = 'doors'
-ACTIVATION_SECONDS = 1
-AUTH_SERVER_URL = 'http://10.100.200.100:3000'
-AUTH_SERVER_URL = 'http://localhost:3000'
 
 port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 
 
 def activate_resource():
     GPIO.output(11, GPIO.HIGH)
-    time.sleep(ACTIVATION_SECONDS)
+    time.sleep(settings['activation_seconds'])
     GPIO.output(11, GPIO.LOW)
 
 while True:
@@ -26,7 +30,7 @@ while True:
         wiegand_tag = serial_to_wiegand(input)
         try:
             response = urllib2.urlopen(
-                '%s/auth/%s/%s' % (AUTH_SERVER_URL, RESOURCE, wiegand_tag)
+                '%s/auth/%s/%s' % (settings['auth_server_url'], settings['resource'], wiegand_tag)
             )
         except urllib2.HTTPError as response:
             if response.code != 401:
